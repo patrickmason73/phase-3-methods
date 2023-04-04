aa36158a3e4106d952eb8d0c12573e8cb6202c9d
Patrick Mason <patrickmason73@gmail.com>
2023-04-03 21:22:52 -0600

    commit tree a0f5a33005e2bdc5519a03598ab9772f006ff079

--- methods.rb
+++ methods.rb
@@ -1,1 +1,23 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet(param)
+    puts "Hello, #{param}!"
+end 
+
+def greet_with_default(param = "programmer")
+    puts "Hello, #{param}!"
+end 
+
+def add(num1, num2)
+    num1 + num2
+end 
+
+def halve(param)
+    if param.class != Integer
+        return nil
+    end
+    param / 2 
+end
